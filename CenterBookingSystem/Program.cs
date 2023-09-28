9a442a365c056179a36f632d54a61336e18415f7
Iamneo <techadmin@iamneo.ai>
2023-09-28 11:43:32 +0000

    Backup Commit

--- CenterBookingSystem/Program.cs
+++ CenterBookingSystem/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+var connectionString = builder.Configuration.GetConnectionString("DevConnection");
+builder.Services.AddDbContext<BookingDbContext>(options => options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
